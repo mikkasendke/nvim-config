c7bea9eb2d359879f5b43b7109be8045b20a5861
Mikka <mikka@sendke.eu>
2023-11-11 22:08:49 +0100

    Telescope background is now transparent

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -7,5 +7,8 @@ return {
 
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
     end
 }
