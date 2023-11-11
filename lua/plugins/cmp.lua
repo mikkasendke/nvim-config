761e9d1a6de6e70f5aec84d8ad3dbf1fa3706f44
Mikka <mikka@sendke.eu>
2023-11-11 21:40:43 +0100

    Disabled copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,7 +3,7 @@ return {
     config = function ()
         require'cmp'.setup {
             sources = {
-                { name = "copilot", group_index = 2 },
+                -- { name = "copilot", group_index = 2 },
                 { name = "nvim_lsp", group_index = 2 },
                 { name = "path", group_index = 2 },
                 { name = "luasnip", group_index = 2 },
