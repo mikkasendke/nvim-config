2c39ab37cf0c60d6bd8bc2ea0dde890fe7bdd25d
Mikka <mikka@sendke.eu>
2024-05-19 00:11:31 +0200

    colors yes

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,35 +1,58 @@
-local colorscheme = "rose-pine"
+-- local colorscheme = "rose-pine"
+local colorscheme = "gruvbox"
+-- local colorscheme = "tokyonight-moon"
+local function standard_colorscheme()
+    vim.cmd("colorscheme " .. colorscheme)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+
+    vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "none" })
+end
 return {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-        require("rose-pine").setup({
+    {
+        "rose-pine/neovim",
+        enabled = colorscheme == "rose-pine",
+        config = standard_colorscheme,
+        opts = {
             disable_italics = true,
             disable_background = true,
             styles = {
                 italic = false,
             }
-        })
-
-        vim.cmd('colorscheme ' .. colorscheme)
-
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
-
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-    end,
-    dependencies = {
-        {
-            "nvim-lualine/lualine.nvim",
-            enabled = false,
         },
-        {
-            "ellisonleao/gruvbox.nvim",
-            enabled = colorscheme == "gruvbox",
-        },
-    }
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        enabled = colorscheme == "gruvbox",
+        config = standard_colorscheme,
+    },
+    {
+        "folke/tokyonight.nvim",
+        enabled = colorscheme == "tokyonight-moon",
+        config = function()
+            require("tokyonight").setup({
+                style = "moon",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                },
+            })
+            standard_colorscheme()
+        end
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        enabled = false,
+    },
 }
